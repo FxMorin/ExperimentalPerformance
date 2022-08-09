920845cf690224219074b1c9a1847e83f5a3610b
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-08-09 03:10:24 -0400

    Tester cleanup

--- src/test/java/classAnalysis/TestAnalysis.java
+++ src/test/java/classAnalysis/TestAnalysis.java
@@ -10,21 +10,12 @@ public class TestAnalysis {
     public static void main(String[] args) {
         // TODO: Fix compiler optimizations getting rid of all the private fields xD
         System.out.println("Starting Class Analysis Test");
-        long startTime = System.nanoTime();
-        int count = 0;
-        for (int i = 0; i < 100000; i++) {
-            //classAnalysisManager.runSingleAnalysis(NothingBoi.class);
-            //classAnalysisManager.runSingleAnalysis(PrivateBoi.class);
-            //classAnalysisManager.runSingleAnalysis(BigBoi.class);
-            //classAnalysisManager.runSingleAnalysis(SmallBoi.class);
-            classAnalysisManager.runSingleAnalysis(TinyBoi.class);
-            count++;
-        }
-        System.out.println("Attempting to generate data, this may take some time - " + count);
-        //classAnalysisManager.printResults();
+        classAnalysisManager.runSingleAnalysis(NothingBoi.class);
+        classAnalysisManager.runSingleAnalysis(PrivateBoi.class);
+        classAnalysisManager.runSingleAnalysis(BigBoi.class);
+        classAnalysisManager.runSingleAnalysis(SmallBoi.class);
+        classAnalysisManager.runSingleAnalysis(TinyBoi.class);
         classAnalysisManager.simulateResults();
-        long duration = System.nanoTime() - startTime;
-        double seconds = (double)duration/1000000000;
-        System.out.println("Done - Took: " + seconds + " seconds");
+        System.out.println("Done");
     }
 }
